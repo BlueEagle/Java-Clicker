de631793e354947089c01308a492217a571949cd
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-03-04 15:32:42 -0700

    Added code to collect and operate on screen size

--- Clicker.java
+++ Clicker.java
@@ -1,17 +1,30 @@
 import java.awt.Robot;
 import java.awt.AWTException;
+import java.awt.Toolkit;
+import java.awt.Dimension;
 
 public class Clicker {
 
     public static void main(String[] args) {
-        System.out.println("This had better work....");
         try{
-            System.out.println("making clicker");
+            // make robot object
             Robot bobTheClicker = new Robot();
 
-            System.out.println("clicker formed!");
-            bobTheClicker.mouseMove(1, 1);
-            System.out.print("worked!");
+            // gather & print screen data
+            System.out.println("Gathering screen dimensions");
+            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+            int screenHeight = screenSize.height;
+            int screenWidth = screenSize.width;
+            System.out.println("Screen width: " + screenWidth + ", height: " + screenHeight + "\n\n");
+            int[] centerPoint = {screenWidth/2, screenHeight/2};
+            
+
+            System.out.println("Circle mode activate!");
+            bobTheClicker.mouseMove(centerPoint[0], centerPoint[1]);
+            
+
+
+
         } catch (Exception e) { System.out.print(e); }
     }
 
