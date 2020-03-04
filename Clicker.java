5d23cfdbf5e689fc7e51cffb62a207bfc48dd3a1
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-03-04 16:02:40 -0700

    Added fun loop that moves my mouse in a perfect centered circle on my screen 100 times.

--- Clicker.java
+++ Clicker.java
@@ -21,7 +21,25 @@ public class Clicker {
 
             System.out.println("Circle mode activate!");
             bobTheClicker.mouseMove(centerPoint[0], centerPoint[1]);
-            
+
+            for(int i=0; i<100;i++){
+            double radius = 200;
+            double theta = 0;
+            double h = centerPoint[0];
+            double k = centerPoint[1];
+            double step = 1;
+
+            System.out.println("About to do the thing!");
+            while(theta++ <= 360) {
+                int[] circleCoord = {
+                    (int)(h + radius*Math.cos(theta)),
+                    (int)(k + radius*Math.sin(theta))
+                };
+                System.out.println(circleCoord[0] + "   " + circleCoord[1]);
+                bobTheClicker.mouseMove(circleCoord[0], circleCoord[1]);
+            }
+            }
+
 
 
 
