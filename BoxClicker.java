ade761133d7a08e5d322cd4d5a1d66cf53f8ca45
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-04-19 23:10:45 -0600

    Minor refinements, It runs faster.

--- BoxClicker.java
+++ BoxClicker.java
@@ -6,42 +6,40 @@ import java.awt.event.InputEvent;
 
 public class BoxClicker {
 
-    public static void main(String[] args) {
-        try{
+    public static void main(final String[] args) {
+        try {
             // make robot object
-            Robot bobTheClicker = new Robot();
+            final Robot bobTheClicker = new Robot();
 
             // gather & print screen data
             System.out.println("Gathering screen dimensions");
-            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-            int screenHeight = screenSize.height;
-            int screenWidth = screenSize.width;
+            final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+            final int screenHeight = screenSize.height;
+            final int screenWidth = screenSize.width;
             System.out.println("Screen width: " + screenWidth + ", height: " + screenHeight + "\n\n");
-            int[] centerPoint = {screenWidth/2, screenHeight/2};
-            
+            final int[] centerPoint = { screenWidth / 2, screenHeight / 2 };
 
             System.out.println("Circle mode activate!");
             bobTheClicker.mouseMove(centerPoint[0], centerPoint[1]);
 
-            for(int i=0; i<100;i++){
-                double radius = 100;
+            for (int i = 0; i < 100; i++) {
+                final double radius = 100;
                 double theta = 0;
-                double h = centerPoint[0];
-                double k = centerPoint[1];
-                double step = (double)((int)(Math.random()*50 + 1));;
+                final double h = centerPoint[0];
+                final double k = centerPoint[1];
+                double step = (double) ((int) (Math.random() * 50 + 1));
+                ;
 
                 System.out.println("About to do the thing!");
-                while(theta <= 360) {
-                    int[] circleCoord = {
-                        (int)(h + radius*Math.cos(theta)),
-                        (int)(k + radius*Math.sin(theta))
-                    };
+                while (theta <= 360) {
+                    final int[] circleCoord = { (int) (h + radius * Math.cos(theta)),
+                            (int) (k + radius * Math.sin(theta)) };
                     System.out.println(circleCoord[0] + "   " + circleCoord[1]);
                     bobTheClicker.mouseMove(circleCoord[0], circleCoord[1]);
-                    Thread.sleep((int)(Math.random()*200 + 51));
+                    Thread.sleep((int) (Math.random() * 70 + 31));
 
                     // sprinkle in some randomness
-                    step = (double)((int)(Math.random()*50 + 1));
+                    step = (double) ((int) (Math.random() * 50 + 1));
 
                     // click
                     bobTheClicker.mousePress(InputEvent.BUTTON1_MASK);
@@ -50,12 +48,10 @@ public class BoxClicker {
                     theta += step;
                 }
             }
-           
 
-
-
-
-        } catch (Exception e) { System.out.print(e); }
+        } catch (final Exception e) {
+            System.out.print(e);
+        }
     }
 
 }
